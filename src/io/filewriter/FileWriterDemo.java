b334be8e7378680526d66e80dd3b88e9620619bf
CaoGongHui <847781669@qq.com>
2018-05-11 15:58:41 +0800

    a package of iostream

--- src/io/filewriter/FileWriterDemo.java
+++ src/io/filewriter/FileWriterDemo.java
@@ -1,4 +1,14 @@
 package io.filewriter;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriterDemo {
+    public static void main(String[] args) throws IOException {
+        FileWriter fileWriter=new FileWriter("demo.txt");//文件不存在自动创建，文件存在则会被覆盖；
+        fileWriter.write("abcde");//写入到临时存储缓冲区中；
+        fileWriter.flush();//进行刷新，将数据直接写到目的地中；
+
+
+    }
 }
